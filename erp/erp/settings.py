453d1bd83ee9b1d2c772f1daf05aca371a6b5c96
lakshman2003 <lakshmankattunga@gmail.com>
2023-04-09 18:44:56 +0530

    commit4

--- erp/erp/settings.py
+++ erp/erp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-r*s!3c%ns#uset3bsf_!w6p&&_fvotfncqvzxt1%@ezjxd%i)+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
